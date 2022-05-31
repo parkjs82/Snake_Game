9c781971876639a43e4e1de4b46943a9ef09baf3
xorud127 <54922652+xorud127@users.noreply.github.com>
2022-06-01 00:36:44 +0900

    Update main.cpp
    add item

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void drawField(makeField& F,snake& S){
     F.field[S.baem[i].row][S.baem[i].colunm] = 3;
   }
   F.field[S.baem[0].row][S.baem[0].colunm]++;
-  
+
   for(int i = 0; i< F.getHeigth();i++){
     move(i+1,2);
     for(int j = 0;j<F.getWidth();j++){
@@ -37,33 +37,25 @@ initShape();
 while(true){
   if(S.gameOver())
     break;
-  
+
+  if(F.field[S.baem[0].row][S.baem[0].colunm] == 5){
+    S.growthLength();
+  }
+  if(F.field[S.baem[0].row][S.baem[0].colunm] == 6){
+    S.reduceLength();
+  }
+
   clear();
 
   drawField(F, S);
-  /*
-  for(int i = 0;i<S.length;i++){
-    F.field[S.baem[i].row][S.baem[i].colunm] = 3;
-  }
-  F.field[S.baem[0].row][S.baem[0].colunm]++;
   
-  for(int i = 0; i< F.getHeigth();i++){
-    move(i+1,2);
-    for(int j = 0;j<F.getWidth();j++){
-      if (F.field[i][j] == 0){appendEmptySpace();}
-      else if(F.field[i][j] == 1 || F.field[i][j] == 2){appendWall();}
-      else if(F.field[i][j] == 3){appendSnakeHead();}
-      else if(F.field[i][j] == 4){appendSnakeBody();}
-      else if(F.field[i][j] == 5){appendGrowthItem();}
-      else if(F.field[i][j] == 6){appendPoisonItem();}
-      else if(F.field[i][j] == 7){appendDoor();}
-    }
-  }
-  */
   refresh();
   for(int i = 0;i<S.length;i++){
     F.field[S.baem[i].row][S.baem[i].colunm] = 0;
   }
+
+  F.field[11][11] = 5;
+  F.field[9][9] = 6;
 
   int pressedKey = getch();
   if(pressedKey == 'w'){
@@ -82,7 +74,6 @@ while(true){
     break;
   }
 }
-
 endwin(); // Curses모드종료
 return 0;
 }
