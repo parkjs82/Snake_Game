0470b9a98c92998e8ea617164f266e3a20a70308
xorud127 <54922652+xorud127@users.noreply.github.com>
2022-06-06 03:54:42 +0900

    upgrade mission

--- Score.cpp
+++ Score.cpp
@@ -4,13 +4,14 @@ Score::Score()
 {
     second = 0.0;
     growth = posion = gate = 0;
+    len = 3;
 }
 
 void Score::time() { second += 0.5; }
 
-void Score::growthItems() { growth += 1; }
+void Score::growthItems() { growth += 1; len += 1; }
 
-void Score::posionItems() { posion += 1; }
+void Score::posionItems() { posion += 1; len -= 1; }
 
 void Score::useGate() { gate += 1; }
 
@@ -20,6 +21,8 @@ int Score::getPosion() { return posion; }
 
 int Score::getGate() { return gate; }
 
-int Score::getScore() { 
-    return second + 10 * (growth + posion + gate); 
+int Score::getLen() { return len; }
+
+int Score::getScore() {
+    return second + 10 * (growth + posion + gate);
 }
