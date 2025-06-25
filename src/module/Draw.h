a5bb50fa424ebbf8732852bf914d5fe6ef5e52b8
parkjs82 <000parkjs@gmail.com>
2025-06-25 17:59:02 +0900

    add: blank

--- src/module/Draw.h
+++ src/module/Draw.h
@@ -10,7 +10,9 @@
 #include "Snake.h"
 
 void drawScore(WINDOW *scoreBoard, Score &score, char nickname[]);
+
 void drawMission(WINDOW *missionBoard, Score &score, int hard);
+
 void drawField(MakeField &field, Snake &snake);
 
 #endif
