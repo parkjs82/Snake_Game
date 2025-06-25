eff2e360cde0b2fd6d5f23481b0460a02e0a9a96
parkjs82 <000parkjs@gmail.com>
2025-06-25 17:27:33 +0900

    add: implement event management functions in Event module

---
+++ src/module/Event.h
@@ -0,0 +1,11 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "MakeField.h"
+#include "Score.h"
+#include "Snake.h"
+
+void eventManager(MakeField &field, Snake &snake, Score &score);
+void makeEvent(MakeField &field, Snake &snake, Score &score);
+
+#endif  // EVENT_H
